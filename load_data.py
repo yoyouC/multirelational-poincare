fa7a3f6a160bbcd39270fe5d6560624d6422e245
Cheng Xuyou <chengxuyou@MacBook-Pro.local>
2023-05-01 23:27:23 +0800

    fix

--- load_data.py
+++ load_data.py
@@ -1,6 +1,7 @@
 class Data:
 
     def __init__(self, data_dir="data/WN18RR/", reverse=True):
+        self.reverse = reverse
         self.train_data = self.load_data(data_dir, "train")
         self.valid_data = self.load_data(data_dir, "valid")
         self.test_data = self.load_data(data_dir, "test")
@@ -12,8 +13,7 @@ class Data:
         self.relations = self.train_relations + [i for i in self.valid_relations \
                 if i not in self.train_relations] + [i for i in self.test_relations \
                 if i not in self.train_relations]
-        self.reverse = reverse
-
+        
     def load_data(self, data_dir, data_type="train"):
         with open("%s%s.txt" % (data_dir, data_type), "r", encoding="utf-8") as f:
             data = f.read().strip().split("\n")
